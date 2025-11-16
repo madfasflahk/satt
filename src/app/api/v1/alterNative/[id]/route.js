2cf7fa33fc64df7bde8c5b6385ce6f32c28d6ea7
Manash-%%%% <manashjbarai@gmail.com>
2025-11-16 18:26:30 +0530

    fox

--- src/app/api/v1/alterNative/[id]/route.js
+++ src/app/api/v1/alterNative/[id]/route.js
@@ -1,34 +1,55 @@
-import dbConnect from '@/lib/db';
-import SattaKingAlterNative from '@/models/alterNativeSattaKing';
-import { NextResponse } from 'next/server';
+import dbConnect from "@/lib/db";
+import SattaKingAlterNative from "@/models/alterNativeSattaKing";
+import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
   await dbConnect();
+
   try {
     const { id } = params;
     const body = await request.json();
-    const updatedEntry = await SattaKingAlterNative.findByIdAndUpdate(id, { $set: body }, { new: true });
+
+    const updatedEntry = await SattaKingAlterNative.findByIdAndUpdate(
+      id,
+      { $set: body },
+      { new: true }
+    );
+
     if (!updatedEntry) {
-      return NextResponse.json({ message: 'Entry not found' }, { status: 404 });
+      return NextResponse.json({ message: "Entry not found" }, { status: 404 });
     }
+
     return NextResponse.json(updatedEntry, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Error updating entry' }, { status: 500 });
+    return NextResponse.json(
+      { message: "Error updating entry" },
+      { status: 500 }
+    );
   }
 }
 
 export async function DELETE(request, { params }) {
   await dbConnect();
+
   try {
     const { id } = params;
+
     const deletedEntry = await SattaKingAlterNative.findByIdAndDelete(id);
+
     if (!deletedEntry) {
-      return NextResponse.json({ message: 'Entry not found' }, { status: 404 });
+      return NextResponse.json({ message: "Entry not found" }, { status: 404 });
     }
-    return NextResponse.json({ message: 'Delete successfully' }, { status: 200 });
+
+    return NextResponse.json(
+      { message: "Delete successfully" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Error deleting entry' }, { status: 500 });
+    return NextResponse.json(
+      { message: "Error deleting entry" },
+      { status: 500 }
+    );
   }
 }
