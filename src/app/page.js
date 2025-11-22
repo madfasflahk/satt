032be846560ad3bd442146ae692136749f2a5111
Manash-%%%% <manashjbarai@gmail.com>
2025-11-22 20:30:13 +0530

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -124,7 +124,7 @@ const HomePage = async () => {
 
 
 
-        <CurrentResult resultOrder={resultOrder} />
+        {/* <CurrentResult resultOrder={resultOrder} /> */}
 
 
         {freeAd.slice(-1).map((e) => (
